077e15f56f49398c942c1fc0c2e3dcda4d6580c9
paul.vautier <sdt@qlik.com>
2024-12-03 21:05:44 +0100

    3.2 with parser

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,48 +1,43 @@
 use pepser::{
-    errors::{ErrorSource, ParserError},
-    impls::{any, none_of, not, sequence, take_while, ws},
-    traits::{discard, opt, parse_if, sep_by, value, wrapped, ParseResult, Parser},
+    impls::{eof, sequence, take_while},
+    traits::{discard, drop_until, wrapped, Input, ParseResult, Parser},
 };
 const FILE_CONTENT: &str = include_str!("input.txt");
 fn main() {
     let content: String = String::from(FILE_CONTENT);
-    let muls = parse(content.as_str()).unwrap().1;
+    let muls = parse_first(content.as_str()).unwrap().1;
 
-    println!("{:?}", muls);
+    println!("{:?}", muls.into_iter().fold(0, |sum, (a, b)| sum + a * b));
+
+    let muls = parse_second(content.as_str()).unwrap().1;
+
     println!("{:?}", muls.into_iter().fold(0, |sum, (a, b)| sum + a * b))
 }
 
+fn mul<'a>() -> impl Parser<&'a str, Output = (i32, i32)> {
+    wrapped(
+        sequence("mul("),
+        digit().and(discard(sequence(","), digit())),
+        sequence(")"),
+    )
+}
 fn digit<'a>() -> impl Parser<&'a str, Output = i32> {
     take_while(|c| c.is_digit(10))
         .map(str::parse::<i32>)
         .map(Result::unwrap)
 }
-fn parse<'a>(input: &'a str) -> ParseResult<&'a str, Vec<(i32, i32)>> {
-    return mul()
-        .or(take_any().map(|_| Vec::<(i32, i32)>::new()))
-        .many()
-        .map(|vecs| vecs.into_iter().flatten().collect::<Vec<(i32, i32)>>())
-        .parse(input);
+
+fn parse_first<'a>(input: &'a str) -> ParseResult<&'a str, Vec<(i32, i32)>> {
+    return drop_until(mul()).many().parse(input);
 }
 
-fn mul<'a>() -> impl Parser<&'a str, Output = Vec<(i32, i32)>> {
-    return wrapped(
-        sequence("mul("),
-        digit().and(discard(sequence(","), digit())),
-        sequence(")"),
+fn parse_second<'a>(input: &'a str) -> ParseResult<&'a str, Vec<(i32, i32)>> {
+    return drop_until(
+        mul().map(|tuple| Some(tuple)).or(sequence("don't()")
+            .and(drop_until(sequence("do()").map(|_| ()).or(eof())))
+            .map(|_| None)),
     )
-    .map(|tuple| vec![tuple]);
-}
-fn take_any<'a>() -> impl Parser<&'a str, Output = &'a str> {
-    return |input| {
-        let mut seen: bool = true;
-        return take_while(move |c| {
-            if (seen) {
-                seen = false;
-                return true;
-            }
-            return false;
-        })
-        .parse(input);
-    };
+    .many()
+    .map(|vecs| vecs.into_iter().flatten().collect::<Vec<(i32, i32)>>())
+    .parse(input);
 }
